371932bd5b094606ee99e3de30a4d22e4e46fc2a
Richard Schoen <richard@richardschoen.net>
2024-04-12 16:46:57 -0500

    Create config.ps1

---
+++ powershell/config.ps1
@@ -0,0 +1,9 @@
+# Appian Deployment Config Parameters
+# API Key
+$apikey="YourAPIKeyHere"
+# Source site URL
+$siteurl="https://yoursourcesitename.appiancloud.com"
+# Dest site URL
+$destsiteurl="https://yourdestsitename.appiancloud.com"
+# Sample App ID
+$appidsample="YourAppIdHereIfDesired"
